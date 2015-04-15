958bd9c8e7dd58bb5324c04ada144bf04e603e2d
Chitrang Singhvi <chitrang3094@gmail.com>
2015-04-16 02:09:09 +0530

    Added a different class

--- Deadlock/src/Main.java
+++ Deadlock/src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+
 
 
 public class Main {
@@ -6,58 +6,32 @@ public class Main {
 	private int[][]R=new int[4][4];
 	private int[][]Alloc=new int[4][4];
 	
+	static Process []p=new Process[4] ;
 	
 		
 	public static void main(String[] args){
-		 Process []p=new Process[4] ;
+		 Main n=new Main();
 		//p=new Process[4];
 		for(int i=0;i<4;i++){
-			p[i].new Process(i);
+			p[i]=new Process(i+1);
 		}
+		n.set();
 	}
 	public boolean check(){
+		set();
 		for(int i=0;i<4;i++){
-			//R[i]=
-		}
-	}
-	
-	public class Process extends Thread{
-		int id;
-		int[] R=new int[4];
-		int[] Alloc=new int[4];
-		Random m=new Random();
-		Process(int id){
-			this.id=id;
-			for(int i=0;i<4;i++){
-				R[i]=m.nextInt(5);
-				Alloc[i]=0;
-			}
-			
-		}
-		public void run(){
-			while(true){
-				int temp=m.nextInt(4);
-				R[temp]=R[temp]-1;
-				Alloc[temp]+=1;
-				boolean wait=check();
-				if(wait==true){
-					while(true){
-						R[temp]=R[temp]+1;
-						Alloc[temp]-=1;
-						if(wait=check()==false){
-							break;
-						}
-					}
-				}
+			if(R[i][0]<=A[0] && R[i][1]<=A[1] && R[i][2]<=A[2] && R[i][3]<=A[3]){
+				return true;
 			}
 		}
-		public int[] getRequired(){
-			return R;
-		}
-		public int[] getAllocated(){
-			return Alloc;
-		}
+		return false;
 	}
 	
+	public void set(){
+		for(int i=0;i<4;i++){
+			R[i]=p[i].getRequired();
+			Alloc[i]=p[i].getAllocated();
+		}
+	}
 	
 }
